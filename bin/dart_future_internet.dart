29476552ca3ee3f1517dc208283ad2b305adff74
carlstep <carlstep333@me.com>
2022-07-20 14:46:01 +0700

    access and use  JSON data from external source

--- bin/dart_future_internet.dart
+++ bin/dart_future_internet.dart
@@ -7,12 +7,20 @@ import 'package:http/http.dart';
 void main(List<String> arguments) async {
   var data = await fetchData();
 
-  print(data);
+  //print(data['books'][2]['title']);
+  //print('${data['books'][2]['title']} has ${data['books'][2]['pages']} pages');
+
+  List books = data['books'];
+  for (int i = 0; i < books.length; i++) {
+    print('');
+    print('Title ----> ${books[i]['title']} (${books[i]['pages']} pages)');
+    print('${books[i]['website']}');
+  }
 }
 
 Future<Map<String, dynamic>> fetchData() async {
   final response = await get(
-      Uri.parse('https://dl.dropbox.com/s/3xj2mgw6q8ieazh/test.json?dl=0'));
+      Uri.parse('https://dl.dropbox.com/s/yhwofe08r459zf5/stories.json?dl=0'));
 
   if (response.statusCode == 200) {
     return jsonDecode(response.body);
